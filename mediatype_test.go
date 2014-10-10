d6f2f51c1833a474ce0d00cfa85b47c2f191e3f8
Trevor Suarez <rican7@gmail.com>
2014-10-10 14:09:01 -0400

    Initial tests

---
+++ mediatype_test.go
@@ -0,0 +1,146 @@
+/**
+ * MediaType
+ *
+ * Copyright © 2014 Trevor N. Suarez (Rican7)
+ */
+
+package mediatype
+
+import (
+	"testing"
+)
+
+/**
+ * Variables
+ */
+
+var validComplexMediaType = "application/vnd.google-earth.kml+xml; charset=utf-8"
+
+var validMediaTypeStrings = []string{
+	"example",
+	"application/json",
+	"application/xhtml+xml",
+	"audio/vnd.rn-realaudio",
+	"image/vnd.djvu",
+}
+
+var invalidMediaTypeStrings = []string{
+	"application/vnd/json",
+	"invalid/+json.really/sick",
+}
+
+/**
+ * Helper functions
+ */
+
+// Parse our valid media types
+func parseValidMediaTypes() (map[string]MediaType, map[string]error) {
+	mediaTypes := make(map[string]MediaType)
+	errors := make(map[string]error)
+
+	for _, val := range validMediaTypeStrings {
+		mt, err := Parse(val)
+
+		if nil != mt {
+			mediaTypes[val] = mt
+		}
+
+		if nil != err {
+			errors[val] = err
+		}
+	}
+
+	return mediaTypes, errors
+}
+
+// Parse our invalid media types
+func parseInvalidMediaTypes() (map[string]MediaType, map[string]error) {
+	mediaTypes := make(map[string]MediaType)
+	errors := make(map[string]error)
+
+	for _, val := range invalidMediaTypeStrings {
+		mt, err := Parse(val)
+
+		if nil != mt {
+			mediaTypes[val] = mt
+		}
+
+		if nil != err {
+			errors[val] = err
+		}
+	}
+
+	return mediaTypes, errors
+}
+
+/**
+ * Tests functions
+ */
+
+func TestParse(t *testing.T) {
+	_, validErrs := parseValidMediaTypes()
+	invalid, _ := parseInvalidMediaTypes()
+
+	for key, err := range validErrs {
+		t.Errorf("Parsing failed for valid '%s' with error '%s'", key, err)
+	}
+
+	for key, _ := range invalid {
+		t.Errorf("Parsing succeeded for invalid '%s'", key)
+	}
+}
+
+func TestMainType(t *testing.T) {
+	mt, err := Parse(validComplexMediaType)
+
+	if nil != err {
+		t.Errorf("Parsing failed for valid '%s'", validComplexMediaType)
+	} else {
+
+		if mt.MainType() != "application" {
+			t.Errorf("Incorrect main type for %+v", mt)
+		}
+	}
+}
+
+func TestSubType(t *testing.T) {
+	mt, err := Parse(validComplexMediaType)
+
+	if nil != err {
+		t.Errorf("Parsing failed for valid '%s'", validComplexMediaType)
+	} else {
+
+		if mt.SubType() != "kml" {
+			t.Errorf("Incorrect sub type for %+v", mt)
+		}
+	}
+}
+
+func TestTrees(t *testing.T) {
+	mt, err := Parse(validComplexMediaType)
+
+	if nil != err {
+		t.Errorf("Parsing failed for valid '%s'", validComplexMediaType)
+	} else {
+		correctTrees := []string{"vnd", "google-earth"}
+
+		for i, tree := range mt.Trees() {
+			if tree != correctTrees[i] {
+				t.Errorf("Incorrect trees for %+v", mt)
+			}
+		}
+	}
+}
+
+func TestSuffix(t *testing.T) {
+	mt, err := Parse(validComplexMediaType)
+
+	if nil != err {
+		t.Errorf("Parsing failed for valid '%s'", validComplexMediaType)
+	} else {
+
+		if mt.Suffix() != "xml" {
+			t.Errorf("Incorrect suffix for %+v", mt)
+		}
+	}
+}
